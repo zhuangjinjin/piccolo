73394a80548c5ef18138450b725ddaa86d38be78
ukuz90 <zhuangjinjin90@gmail.com>
2021-10-11 20:03:26 +0800

    feat: add RocketMQ support

---
+++ piccolo-mq/src/main/java/io/github/ukuz/piccolo/mq/rocketmq/RocketMQClient.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright 2019 ukuz90
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.github.ukuz.piccolo.mq.rocketmq;
+
+import io.github.ukuz.piccolo.api.PiccoloContext;
+import io.github.ukuz.piccolo.api.mq.MQClient;
+import io.github.ukuz.piccolo.api.mq.MQMessage;
+import io.github.ukuz.piccolo.api.mq.MQMessageReceiver;
+import io.github.ukuz.piccolo.api.mq.MQTopic;
+import io.github.ukuz.piccolo.api.service.AbstractService;
+import io.github.ukuz.piccolo.api.service.ServiceException;
+import io.github.ukuz.piccolo.mq.kafka.KafkaMqMessage;
+import io.github.ukuz.piccolo.mq.kafka.manager.KafkaManager;
+import org.apache.kafka.clients.consumer.OffsetAndMetadata;
+import org.apache.kafka.common.TopicPartition;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.CompletableFuture;
+
+/**
+ * @author ukuz90
+ */
+public class RocketMQClient extends AbstractService implements MQClient {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(RocketMQClient.class);
+    private KafkaManager kafkaManager;
+
+    @Override
+    public void init(PiccoloContext context) throws ServiceException {
+        kafkaManager = new KafkaManager(context);
+        kafkaManager.init();
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    protected CompletableFuture<Boolean> doStartAsync() {
+        CompletableFuture future = new CompletableFuture();
+        future.complete(true);
+        return future;
+    }
+
+    @Override
+    public void destroy() throws ServiceException {
+        kafkaManager.destroy();
+    }
+
+    @Override
+    public void addTopicIfNeeded(MQTopic topic) {
+        kafkaManager.addTopicIfNeeded(topic);
+    }
+
+    @Override
+    public void subscribe(String topic, MQMessageReceiver receiver) {
+        kafkaManager.subscribe(topic, receiver);
+    }
+
+    @Override
+    public void publish(String topic, Object message) {
+        publish(topic, null, message);
+    }
+
+    @Override
+    public void publish(String topic, String key, Object message) {
+        if (message instanceof String) {
+            kafkaManager.publish(topic, key, ((String) message).getBytes(StandardCharsets.UTF_8));
+        } else if (message instanceof byte[]) {
+            kafkaManager.publish(topic, key, message);
+        } else {
+            LOGGER.warn("publish failure with a unsupported wire type, topic: {}, message: {}", topic, message);
+        }
+    }
+
+    @Override
+    public void commitMessage(MQMessage message) {
+        if (message instanceof RocketMqMessage) {
+            RocketMqMessage kafkaMqMessage = (RocketMqMessage)message;
+            kafkaManager.commitOffset(new TopicPartition(kafkaMqMessage.getTopic(), kafkaMqMessage.getPartition()),
+                    new OffsetAndMetadata(kafkaMqMessage.getOffset()));
+        } else {
+            LOGGER.error("commit unsupported wire type: {}", message);
+        }
+    }
+
+    @Override
+    protected String getName() {
+        return "rocketmq client";
+    }
+}
---
+++ piccolo-mq/src/main/java/io/github/ukuz/piccolo/mq/rocketmq/RocketMqMessage.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2019 ukuz90
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.github.ukuz.piccolo.mq.rocketmq;
+
+import io.github.ukuz.piccolo.api.external.common.Assert;
+import io.github.ukuz.piccolo.mq.CommittableMqMessage;
+
+/**
+ * @author ukuz90
+ */
+public abstract class RocketMqMessage extends CommittableMqMessage {
+
+    private String topic;
+    private int partition;
+    private long offset;
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public int getPartition() {
+        return partition;
+    }
+
+    public void setPartition(int partition) {
+        this.partition = partition;
+    }
+
+    public long getOffset() {
+        return offset;
+    }
+
+    public void setOffset(long offset) {
+        this.offset = offset;
+    }
+
+    @Override
+    public void completeConsume() {
+        Assert.notNull(mqClient, "mqClient must not be null");
+        mqClient.commitMessage(this);
+    }
+
+}
+
